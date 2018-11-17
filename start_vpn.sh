7d270329fa65b43589cadfc503c2a4ff79cde972
zz <zz@cloud.(none)>
2018-11-17 12:02:39 +0800

    Add vps setup file.

---
+++ start_vpn.sh
@@ -0,0 +1,9 @@
+/etc/init.d/shadowsocks start
+# wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-go.sh
+# ./shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
+# /etc/sysconfig/iptables
+# -A INPUT -m state --state NEW -m tcp -p tcp --dport 8989 -j ACCEPT
+# ./shadowsocks-go.sh uninstall
+# /etc/init.d/shadowsocks start
+# config
+# /etc/shadowsocks/config.json
