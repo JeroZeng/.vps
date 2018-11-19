0c2bee4f9c2b3896899052b3f23a58e1a5647571
Jerome <zsmilef.gmail.com>
2018-11-19 20:15:37 +0800

    Add firewall config script.

---
+++ firewall_cfg.sh
@@ -0,0 +1,7 @@
+echo "Start Firewall"
+systemctl start firewalld
+echo "Enable Port 3225"
+firewall-cmd --zone=public --add-port=3225/tcp --permanent
+echo "Reload Firewall"
+sudo firewall-cmd --reload
+echo "Success"
--- start_vpn.sh
+++ start_vpn.sh
@@ -1,6 +1,9 @@
 /etc/init.d/shadowsocks start
 # wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-go.sh
 # ./shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
+
+# open firewall
+#	systemctl start firewalld
 # query port status
 # 	sudo firewall-cmd --query-port=3225/tcp
 # open port
@@ -9,8 +12,9 @@
 #	sudo firewall-cmd --reload
 # close port
 #	sudo firewall-cmd --permanent --remove-port=3225/tcp
+
 # /etc/sysconfig/iptables
-# -A INPUT -m state --state NEW -m tcp -p tcp --dport 8989 -j ACCEPT
+# -A INPUT -m state --state NEW -m tcp -p tcp --dport 3225 -j ACCEPT
 # ./shadowsocks-go.sh uninstall
 # /etc/init.d/shadowsocks start
 # config
