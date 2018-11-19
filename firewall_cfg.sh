b2bca8584a4dabac092b1213c412b61783c542e7
Jerome <zsmilef.gmail.com>
2018-11-19 20:28:36 +0800

    Add script to fetch and setup shadowsocks.

--- firewall_cfg.sh
+++ firewall_cfg.sh
@@ -1,6 +1,10 @@
+echo "Fetch Shadowsocks"
+wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-go.sh
 echo "Start Firewall"
 systemctl start firewalld
 echo "Enable Port 3225"
 firewall-cmd --zone=public --add-port=3225/tcp --permanent
 echo "Reload Firewall"
 sudo firewall-cmd --reload
+echo "Setup Shadowsocks"
+./shadowsocks-go.sh
